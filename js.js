6392e312e75a5ed4c8bc8c47446665c005ef59e2
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-05-17 15:11:19 +0700

    sửa lại

--- js.js
+++ js.js
@@ -1,27 +1,99 @@
+function numOne() {
+    let one = document.getElementById("num1").value;
+    document.getElementById("text").value += one;
+}
+function numTwo() {
+    let two = document.getElementById("num2").value;
+    document.getElementById("text").value += two;
+}
+function numThree() {
+    let three = document.getElementById("num3").value;
+    document.getElementById("text").value += three;
+}
+function numCong() {
+    let addition = document.getElementById("numcong").value;
+    document.getElementById("text").value += addition;
+}
+function numFour() {
+    let four = document.getElementById("num4").value;
+    document.getElementById("text").value += four;
+}
+function numFive() {
+    let five = document.getElementById("num5").value;
+    document.getElementById("text").value += five;
+}
+function numSix() {
+    let six = document.getElementById("num6").value;
+    document.getElementById("text").value += six;
+}
+function numTru() {
+    let subtraction = document.getElementById("numtru").value;
+    document.getElementById("text").value += subtraction;
+}
+function numSeven() {
+    let seven = document.getElementById("num7").value;
+    document.getElementById("text").value += seven;
+}
+function numEight() {
+    let eight = document.getElementById("num8").value;
+    document.getElementById("text").value += eight;
+}
+function numNine() {
+    let nine = document.getElementById("num9").value;
+    document.getElementById("text").value += nine;
+}
+function numNhan() {
+    let munTipLiCaTion = document.getElementById("numnhan").value;
+    document.getElementById("text").value += munTipLiCaTion;
+}
+function numXoa() {
+    let C = document.getElementById("numxoa").value;
+    let xoa = C.slice(99);
+    document.getElementById("text").value = xoa;
+}
+function numZero() {
+    let zero = document.getElementById("num0").value;
+    document.getElementById("text").value += zero;
+}
+function numKetQua() {
+    let result = eval(document.getElementById("text").value);
+    document.getElementById("text").value = result;
+}
+function numChia() {
+    let divition = document.getElementById("numchia").value;
+    document.getElementById("text").value += divition;
+}
 
-function addition() {
-    let a = parseInt(document.getElementById("num1").value);
-    let b = parseInt(document.getElementById("num2").value);
-    let k = a+b;
-    document.getElementById("result").innerHTML = "Result Divition:" + k;
-}
-function subtraction() {
-    let a = parseInt(document.getElementById("num1").value);
-    let b = parseInt(document.getElementById("num2").value);
-    let k = a - b;
-    document.getElementById("result").innerHTML = "Result Divition:" + k;
-}
-function munTipLiCaTion() {
-    let a = parseInt(document.getElementById("num1").value);
-    let b = parseInt(document.getElementById("num2").value);
-    let k = a * b;
-    document.getElementById("result").innerHTML = "Result Divition:" + k;
-}
-function divISion() {
-    let a = parseInt(document.getElementById("num1").value);
-    let b = parseInt(document.getElementById("num2").value);
-    let k = a / b;
-    document.getElementById("result").innerHTML = "Result Divition:" + k;
-}
 
 
+
+
+
+// function addition() {
+//     let a = parseInt(document.getElementById("num1").value);
+//     let b = parseInt(document.getElementById("num2").value);
+//     let k = a+b;
+//     document.getElementById("result").innerHTML = "Result Divition:" + k;
+// }
+// function subtraction() {
+//     let a = parseInt(document.getElementById("num1").value);
+//     let b = parseInt(document.getElementById("num2").value);
+//     let k = a - b;
+//     document.getElementById("result").innerHTML = "Result Divition:" + k;
+// }
+// function munTipLiCaTion() {
+//     let a = parseInt(document.getElementById("num1").value);
+//     let b = parseInt(document.getElementById("num2").value);
+//     let k = a * b;
+//     document.getElementById("result").innerHTML = "Result Divition:" + k;
+// }
+// function divISion() {
+//     let a = parseInt(document.getElementById("num1").value);
+//     let b = parseInt(document.getElementById("num2").value);
+//     let k = a / b;
+//     document.getElementById("result").innerHTML = "Result Divition:" + k;
+// }
+//
+// function inPut() {
+//     let k = document.getElementById("")
+// }
